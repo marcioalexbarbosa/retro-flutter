4c00312dc25f33fc410bfc8e1116e0e7930031e9
Marcio Alexandre Barbosa <m.alexan@hotmail.com>
2021-03-30 11:26:05 -0300

    refactor

--- lib/card_api.dart
+++ lib/card_api.dart
@@ -1,4 +1,4 @@
-import 'card.dart';
+import 'card_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flip/card_api.dart';
-import 'package:flip_card/flip_card.dart';
+import 'package:flip/card_widget.dart';
 import 'package:flutter/material.dart';
 
-import 'card.dart';
+import 'card_model.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -44,7 +44,9 @@ class _HomePageState extends State<HomePage> {
   Row _row(context, cards) {
     var cardsList = <Widget>[];
 
-    cards.forEach((card) => {cardsList.add(_card(context, card))});
+    var cardWidget = CardWidget();
+
+    cards.forEach((card) => {cardsList.add(cardWidget.card(context, card))});
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -76,74 +78,6 @@ class _HomePageState extends State<HomePage> {
     var rows = _rows(context);
 
     return SingleChildScrollView(child: Column(children: rows));
-  }
-
-  Widget _card(context, CardModel card) {
-    return Expanded(
-        flex: 1,
-        child: Card(
-          elevation: 0.0,
-          margin:
-              EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 0.0),
-          color: Color(0x00000000),
-          child: FlipCard(
-            key: UniqueKey(),
-            direction: FlipDirection.HORIZONTAL,
-            speed: 1500,
-            front: Container(
-              height: 200,
-              decoration: BoxDecoration(
-                color: Color.fromARGB(250, 1, 128, 96),
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(card.front,
-                      style: Theme.of(context).textTheme.headline5),
-                  Text('surprise me',
-                      style: Theme.of(context).textTheme.bodyText1),
-                ],
-              ),
-            ),
-            back: Container(
-              height: 200,
-              decoration: BoxDecoration(
-                color: card.serious
-                    ? Color.fromARGB(250, 208, 198, 7)
-                    : Color.fromARGB(250, 75, 210, 45),
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Spacer(),
-                  Text(card.back,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color:
-                              card.serious ? Colors.black : Colors.deepPurple,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold)),
-                  Spacer(),
-                  Container(
-                    padding: const EdgeInsets.all(5.0),
-                    alignment: Alignment.bottomCenter,
-                    child: Image(
-                      image: AssetImage(
-                          card.serious ?
-                          "images/scream.png" :
-                          "images/relief.png"
-                      ),
-                      height: 40,
-                      width: 40,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ));
   }
 
   @override
